114d505f90ee2082cb7be0a30ad52c80fe7c3efb
vdixitav <vishwasraodixita@gmail.com>
2026-02-17 12:26:27 +0530

    gaudril

---
+++ src/guardrails.py
@@ -0,0 +1,12 @@
+# to build safe,filtering content at key point agent execution, detect sensitive info, common usage: preventing PII leakage, harmfull cotent
+
+
+# two cpmplementry approch
+
+#1) deterministic guard:
+
+# buildin: PII detection personal identifiable information
+
+
+
+# pii staergy: hashing, masking,redact,blocking
